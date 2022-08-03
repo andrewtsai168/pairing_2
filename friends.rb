6cb9c5c9f3266e01384ad42d853d5e14d59a9892
Andrew Tsai <a168tsai@gmail.com>
2022-08-03 16:41:22 -0600

    Add elsif for good friend

--- friends.rb
+++ friends.rb
@@ -1,8 +1,11 @@
 best_friend = "hodhod"
-friend = "" # insert friend name here
+friend = "josh" # insert friend name here
+friend_two = "josh"
 
 if friend == best_friend
   print "buy me sushi"
+elsif friend == friend_two
+  print "ask #{best_friend} for sushi"
 else
   print "food is on me"
 end
